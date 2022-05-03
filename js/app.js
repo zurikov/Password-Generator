46f2a707be8ae128d354095ebe4caa041773b49d
zurikov <andrejzurikov@gmail.com>
2022-05-03 15:31:41 +0300

    All files Added

---
+++ js/app.js
@@ -0,0 +1,9 @@
+function genPass() {
+	let passLength = document.getElementById('passLength').value;
+	let passChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0987654321';
+	let userPass = '';
+	for (let i = 0; i < passLength ; i++) {
+		userPass += passChars.charAt(Math.floor(Math.random() * passChars.length));
+	}
+	document.getElementById('userPass').value = userPass;
+}
